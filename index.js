0f8820bbd330dce9f15d80ce8718f9be8bc9fc26
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-26 18:11:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,4 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
-const oldID = "some shit"
-const currentAge = "another shit"
-
-const newID = oldID + 1
+const newID = oldID + 1000000000
